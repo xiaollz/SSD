2fa61877ff940c953be1ed4401421438bddcb8c4
xiaollz <fosterthecloud@me.com>
2019-05-05 10:52:20 +0800

    Update defaults.py

--- ssd/config/defaults.py
+++ ssd/config/defaults.py
@@ -6,31 +6,35 @@ _C.MODEL = CN()
 _C.MODEL.DEVICE = "cuda"
 # match default boxes to any ground truth with jaccard overlap higher than a threshold (0.5)
 _C.MODEL.THRESHOLD = 0.5
-_C.MODEL.NUM_CLASSES = 21
+_C.MODEL.NUM_CLASSES = 81
 # Hard negative mining
 _C.MODEL.NEG_POS_RATIO = 3
 _C.MODEL.CENTER_VARIANCE = 0.1
 _C.MODEL.SIZE_VARIANCE = 0.2
-# -----------------------------------------------------------------------------
-# PRIORS
-# -----------------------------------------------------------------------------
-_C.MODEL.PRIORS = CN()
-_C.MODEL.PRIORS.FEATURE_MAPS = [38, 19, 10, 5, 3, 1]
-_C.MODEL.PRIORS.STRIDES = [8, 16, 32, 64, 100, 300]
-_C.MODEL.PRIORS.MIN_SIZES = [30, 60, 111, 162, 213, 264]
-_C.MODEL.PRIORS.MAX_SIZES = [60, 111, 162, 213, 264, 315]
-_C.MODEL.PRIORS.ASPECT_RATIOS = [[2], [2, 3], [2, 3], [2, 3], [2], [2]]
-# When has 1 aspect ratio, every location has 4 boxes, 2 ratio 6 boxes.
-# #boxes = 2 + #ratio * 2
-_C.MODEL.PRIORS.BOXES_PER_LOCATION = [4, 6, 6, 6, 4, 4]  # number of boxes per feature map location
-_C.MODEL.PRIORS.CLIP = True
+
+_C.MODEL.FCOS_ON = True
+# ---------------------------------------------------------------------------- #
+# FCOS Options
+# ---------------------------------------------------------------------------- #
+_C.MODEL.FCOS = CN()
+_C.MODEL.FCOS.NUM_CLASSES = 81  # the number of classes including background
+_C.MODEL.FCOS.STRIDES = [8, 16, 32, 64, 128, 256]
+_C.MODEL.FCOS.PRIOR_PROB = 0.01
+_C.MODEL.FCOS.INFERENCE_TH = 0.05
+_C.MODEL.FCOS.NMS_TH = 0.6
+_C.MODEL.FCOS.PRE_NMS_TOP_N = 1000
+
+# Focal loss parameter: alpha
+_C.MODEL.FCOS.LOSS_ALPHA = 0.25
+# Focal loss parameter: gamma
+_C.MODEL.FCOS.LOSS_GAMMA = 2.0
 
 # -----------------------------------------------------------------------------
 # INPUT
 # -----------------------------------------------------------------------------
 _C.INPUT = CN()
 # Image size
-_C.INPUT.IMAGE_SIZE = 300
+_C.INPUT.IMAGE_SIZE = 512
 # Values to be used for image normalization, RGB layout
 _C.INPUT.PIXEL_MEAN = [123, 117, 104]
 
@@ -62,8 +66,8 @@ _C.SOLVER.WARMUP_ITERS = 500
 # Specific test options
 # ---------------------------------------------------------------------------- #
 _C.TEST = CN()
-_C.TEST.NMS_THRESHOLD = 0.45
-_C.TEST.CONFIDENCE_THRESHOLD = 0.01
+# _C.TEST.NMS_THRESHOLD = 0.45
+# _C.TEST.CONFIDENCE_THRESHOLD = 0.01
 # change MAX_PER_CLASS to 400 as official caffe code will slightly increase mAP(0.8025=>0.8063, 0.7783=>0.7798)
 _C.TEST.MAX_PER_CLASS = 200
 _C.TEST.MAX_PER_IMAGE = -1
